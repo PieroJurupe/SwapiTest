92dee73694d57f47f21ea3dd388ce2605ea05d08
PieroJurupe <pierojurupeq@gmail.com>
2025-06-19 13:05:36 -0500

    Mostrando personajes en cards y añadiendo clickcables

--- app/src/main/java/com/example/swapi/home/ui/Home.kt
+++ app/src/main/java/com/example/swapi/home/ui/Home.kt
@@ -1,14 +1,21 @@
 package com.example.swapi.home.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.runBlocking
@@ -30,12 +37,20 @@ fun Home(modifier: Modifier) {
 
 @Composable
 fun  CharactersScreen(characters: List<SwapiResponse>, modifier: Modifier) {
-    LazyColumn(modifier = modifier.padding(16.dp)) {
-        items(characters) { character ->
-            Column(modifier = Modifier.padding(vertical = 8.dp)) {
-                Text(text = "Nombre: ${character.name}")
-                Text(text = "Altura: ${character.height}")
+    LazyColumn(modifier = modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            items(characters) { character ->
+                Column(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                    Card(modifier = Modifier.height(250.dp).width(200.dp).clickable{}) {
+                        Column(modifier=Modifier) {
+                            Text("Imagen")
+                        }
+                    }
+                    Text(text = "Nombre: ${character.name}")
+                }
             }
         }
     }
-}
